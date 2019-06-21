e24c8a23064d9920d65a66717a962f9c282617ff
guilleeh <guilleeh@uci.edu>
2019-06-21 15:06:10 -0700

    isLoading state for github requests

--- src/App.js
+++ src/App.js
@@ -1,19 +1,37 @@
 import React from 'react';
 import Navbar from './components/layout/Navbar';
 import Users from './components/users/Users';
+import axios from 'axios';
 import './App.css';
 
 class App extends React.Component {
-    componentDidMount = () => {
-        console.log(123);
+    state = {
+        users: [],
+        loading: false
     };
+
+    async componentDidMount() {
+        this.setState({ loading: true });
+        console.log(process.env.REACT_APP_GITHUB_CLIENT_ID);
+
+        const response = await axios.get(
+            `https://api.github.com/users?client_id=${
+                process.env.REACT_APP_GITHUB_CLIENT_ID
+            }&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
+        );
+
+        this.setState({ users: response.data, loading: false });
+    }
 
     render() {
         return (
             <div className='App'>
                 <Navbar title='Github Finder' icon='fab fa-github' />
                 <div className='container'>
-                    <Users />
+                    <Users
+                        loading={this.state.loading}
+                        users={this.state.users}
+                    />
                 </div>
             </div>
         );
--- src/components/users/UserItem.js
+++ src/components/users/UserItem.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import Users from './Users';
 
 const UserItem = ({ user: { login, avatar_url, html_url } }) => {
     return (
@@ -18,6 +19,10 @@ const UserItem = ({ user: { login, avatar_url, html_url } }) => {
             </div>
         </div>
     );
+};
+
+UserItem.propTypes = {
+    user: PropTypes.object.isRequired
 };
 
 export default UserItem;
