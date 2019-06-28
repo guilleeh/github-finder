4c0d84c53a4d10b011b6594a9ec7637df586e056
guilleeh <guilleeh@uci.edu>
2019-06-28 00:44:52 -0700

    User page created

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import Navbar from './components/layout/Navbar';
 import Users from './components/users/Users';
 import Search from './components/users/Search';
 import Alert from './components/layout/Alert';
+import About from './components/pages/About';
+import User from './components/users/User';
 import axios from 'axios';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import './App.css';
 
 class App extends React.Component {
     state = {
         users: [],
+        user: {},
         loading: false,
         alert: null
     };
@@ -26,9 +30,23 @@ class App extends React.Component {
         this.setState({ users: response.data.items, loading: false });
     };
 
+    // Get single user
+    getUser = async username => {
+        console.log('WHAT');
+        this.setState({ loading: true });
+
+        const response = await axios.get(
+            `https://api.github.com/users/${username}?client_id=${
+                process.env.REACT_APP_GITHUB_CLIENT_ID
+            }&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
+        );
+
+        this.setState({ user: response.data, loading: false });
+    };
+
     // Clear users from state
     clearUsers = () => {
-        this.setState({ users: [], loading: false });
+        this.setState({ user: [], loading: false });
     };
 
     //Set an alert
@@ -40,22 +58,52 @@ class App extends React.Component {
     };
 
     render() {
-        const { users, loading } = this.state;
+        const { users, user, loading } = this.state;
 
         return (
-            <div className='App'>
-                <Navbar title='Github Finder' icon='fab fa-github' />
-                <div className='container'>
-                    <Alert alert={this.state.alert} />
-                    <Search
-                        searchUsers={this.searchUsers}
-                        clearUsers={this.clearUsers}
-                        showClear={users.length > 0 ? true : false}
-                        setAlert={this.setAlert}
-                    />
-                    <Users loading={loading} users={users} />
+            <Router>
+                <div className='App'>
+                    <Navbar title='Github Finder' icon='fab fa-github' />
+                    <div className='container'>
+                        <Alert alert={this.state.alert} />
+                        <Switch>
+                            <Route
+                                exact
+                                path='/'
+                                render={props => (
+                                    <Fragment>
+                                        <Search
+                                            searchUsers={this.searchUsers}
+                                            clearUsers={this.clearUsers}
+                                            showClear={
+                                                users.length > 0 ? true : false
+                                            }
+                                            setAlert={this.setAlert}
+                                        />
+                                        <Users
+                                            loading={loading}
+                                            users={users}
+                                        />
+                                    </Fragment>
+                                )}
+                            />
+                            <Route exact path='/about' component={About} />
+                            <Route
+                                exact
+                                path='/user/:login'
+                                render={props => (
+                                    <User
+                                        {...props}
+                                        getUser={this.getUser}
+                                        user={user}
+                                        loading={loading}
+                                    />
+                                )}
+                            />
+                        </Switch>
+                    </div>
                 </div>
-            </div>
+            </Router>
         );
     }
 }
